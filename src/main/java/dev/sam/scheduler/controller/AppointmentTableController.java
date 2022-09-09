ffe38dcebe2e6a7230440314d9769cb1f0cad0c9
s-waite <samwaite11@gmail.com>
2022-09-09 10:32:12 -0600

    Add javadoc comments

--- src/main/java/dev/sam/scheduler/controller/AppointmentTableController.java
+++ src/main/java/dev/sam/scheduler/controller/AppointmentTableController.java
@@ -22,6 +22,9 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
 
+/**
+ * Controller for the appointment table view.
+ */
 public class AppointmentTableController extends Table implements Controller, Tab, Initializable {
 
     @FXML
@@ -64,16 +67,29 @@ public class AppointmentTableController extends Table implements Controller, Tab
     ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
     FilteredList<Appointment> appointmentFilteredList;
 
+    /**
+     * Set up the nodes for the scene, including the table
+     */
     @Override
     public void initializeNodes() {
-
+        initializeTable(appointmentTable, appointmentFilteredList);
     }
 
+    /**
+     * Set up the click listeners for the buttons in the scene
+     */
     @Override
     public void initializeClickListeners() {
 
     }
 
+    /**
+     * Populate the table and set the cell value factory for each column
+     *
+     * @param tableView  The tableView to populate
+     * @param tableItems The Filtered List of table items to populate the table
+     * @param <T>        The type parameter
+     */
     @Override
     <T> void initializeTable(TableView<T> tableView, List<T> tableItems) {
         super.initializeTable(tableView, tableItems);
@@ -127,6 +143,14 @@ public class AppointmentTableController extends Table implements Controller, Tab
         });
     }
 
+    /**
+     * This function gets called whenever the "appointments" tab is selected.
+     * <p>
+     * If a customer is selected in the SharedData singleton, the function displays only the appointments that match
+     * that customer ID
+     * <p>
+     * Otherwise, the table filter is reset to show all items
+     */
     @Override
     public void onThisTabSelected() {
         Customer activeCustomer = SharedData.INSTANCE.getActiveCustomer();
@@ -139,16 +163,31 @@ public class AppointmentTableController extends Table implements Controller, Tab
         }
     }
 
+    /**
+     * Filter the table items to only show appointments for a certain customer.
+     *
+     * @param customer The customer to show appointments for
+     */
     private void filterTableForCustomer(Customer customer) {
         appointmentFilteredList.setPredicate(a -> a.getCustomerId() == customer.getId());
     }
 
-    // Resets the filter to show all appointments
-    // Refreshes the table if a new appointment was added
+
+    /**
+     * Resets the table filter to show all items.
+     *
+     * If a new appointment was just added to the database, refreshes the table so that new appointment shows up.
+     * @param newItemWasAddedToDb True if a new appointment was just added to the database
+     */
     public void refreshTable(boolean newItemWasAddedToDb) {
         appointmentFilteredList.setPredicate(appointment -> true);
     }
 
+    /**
+     * This function is called after the root elements of the scene has been processed, and sets up the scene when it is loaded
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         SharedData.INSTANCE.setAppointmentTableController(this);
@@ -161,6 +200,6 @@ public class AppointmentTableController extends Table implements Controller, Tab
             throw new RuntimeException(e);
         }
         appointmentsForTextPrompt = appointmentsForText.getText();
-        initializeTable(appointmentTable, appointmentFilteredList);
+        initializeNodes();
     }
 }
